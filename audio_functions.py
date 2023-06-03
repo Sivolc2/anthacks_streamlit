5127a692ab0bc1a8f52df5f56adf831ed1cc8d40
Clovis <ClovisVT@gmail.com>
2023-06-03 16:11:58 -0700

    test audio function

---
+++ audio_functions.py
@@ -0,0 +1,18 @@
+# audio_functions.py
+
+import numpy as np
+import streamlit as st
+
+def generate_audio():
+    # Sample rate 44100 Hz
+    sample_rate = 44100  
+    seconds = 2  # Note duration of 2 seconds
+    frequency_la = 440  # Our played note will be 440 Hz
+
+    # Generate array with seconds*sample_rate steps, ranging between 0 and seconds
+    t = np.linspace(0, seconds, seconds * sample_rate, False)
+
+    # Generate a 440 Hz sine wave
+    note_la = np.sin(frequency_la * t * 2 * np.pi)
+
+    return note_la, sample_rate
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,25 +1,37 @@
 # streamlit_app.py
+
+import os
 import streamlit as st
 from llm_functions import *
+from audio_functions import generate_audio
 
-## Streamlit envvars
-st.write(
-    "Has environment variables been set:",
-    os.environ["ANTHROPIC_API_KEY"] == st.secrets["ANTHROPIC_API_KEY"],
-)
-
-## Utils
 def main():
     # Set a title
     st.title("My LangChain App")
 
-    # Select between the translation and chat pages
-    page = st.sidebar.selectbox("Choose a page:", ["Translation", "Chat"])
+    # Display environment variable information
+    st.write(
+        "Has environment variables been set:",
+        os.environ["ANTHROPIC_API_KEY"] == st.secrets["ANTHROPIC_API_KEY"],
+    )
+
+    # Select between the translation, chat, and audio pages
+    page = st.sidebar.selectbox("Choose a page:", ["Translation", "Chat", "Audio"])
 
     if page == "Translation":
         translation_page()
     elif page == "Chat":
         chat_page()
+    elif page == "Audio":
+        audio_page()
+
+
+def audio_page():
+    # Generate an audio signal
+    audio_signal, sample_rate = generate_audio()
+
+    # Display an audio player
+    st.audio(audio_signal, format='audio/wav', start_time=0, sample_rate=sample_rate)
 
 
 if __name__ == "__main__":
