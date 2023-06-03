3458bfb42bd36ffcc9505ae7bd13a61886e9ec6d
Clovis <ClovisVT@gmail.com>
2023-06-03 13:08:32 -0700

    2 stage

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,26 +6,16 @@ from langchain.chat_models import ChatAnthropic
 from langchain.schema import HumanMessage
 from langchain.callbacks.manager import CallbackManager
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+from anthropic import AnthropicClient
 
-
-## Set Key
-import streamlit as st
-
-import os
-
-# Everything is accessible via the st.secrets dict:
-# And the root-level secrets are also accessible as environment variables:
+## Get Key
 st.write(
     "Has environment variables been set:",
     os.environ["ANTHROPIC_API_KEY"] == st.secrets["ANTHROPIC_API_KEY"],
 )
+anthropic_client = anthropic.Client(api_key=os.getenv("ANTHROPIC_API_KEY"))
 
-## Take user input and run out
-def main():
-
-    # Set a title
-    st.title("My LangChain Translation App")
-
+def translation_page():
     # Ask the user for a sentence to translate
     user_sentence = st.text_input("Please enter a sentence to translate from English to French:")
 
@@ -39,11 +29,44 @@ def main():
         response = chat(messages)
 
         # Display the translation
-        st.write(f"The translated sentence is: {response.content}")
+        st.write(f"The translated sentence is: {response[0].content}")
 
-        # Uncomment the following lines if you want to use streaming and verbosity
-        # chat = ChatAnthropic(streaming=True, verbose=True, callback_manager=CallbackManager([StreamingStdOutCallbackHandler()]))
-        # response = chat(messages)
+
+def chat_page():
+    anthropic_client = AnthropicClient(api_key=os.environ["ANTHROPIC_API_KEY"])
+    anthropic = ChatAnthropic()
+
+    context = ""
+    user_inp = st.text_input("You: ")
+
+    if user_inp:
+        current_inp = anthropic.HUMAN_PROMPT + user_inp + anthropic.AI_PROMPT
+        context += current_inp
+
+        prompt = context
+
+        completion = anthropic_client.completion(
+            prompt=prompt, model="claude-v1.3-100k", max_tokens_to_sample=1000
+        )["completion"]
+
+        context += completion
+
+        # Display the response from the model
+        st.write("Anthropic: " + completion)
+
+
+def main():
+    # Set a title
+    st.title("My LangChain App")
+
+    # Select between the translation and chat pages
+    page = st.sidebar.selectbox("Choose a page:", ["Translation", "Chat"])
+
+    if page == "Translation":
+        translation_page()
+    elif page == "Chat":
+        chat_page()
+
 
 if __name__ == "__main__":
     main()
