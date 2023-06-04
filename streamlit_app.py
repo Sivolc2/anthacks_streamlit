d9507ffb23a05e848a8e07992d522c9f0bb07e70
Clovis <ClovisVT@gmail.com>
2023-06-03 17:33:27 -0700

    test rec

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,37 @@
 # streamlit_app.py
-
-import os
 import streamlit as st
 from llm_functions import *
-from audio_functions import generate_audio
+from st_custom_components import st_audiorec  # Ensure this is available
 
+## Streamlit envvars
+st.write(
+    "Has environment variables been set:",
+    os.environ["ANTHROPIC_API_KEY"] == st.secrets["ANTHROPIC_API_KEY"],
+    os.environ["OPENAI_API_KEY"] == st.secrets["OPENAI_API_KEY"],
+    os.environ["ELEVENLABS_API_KEY"] == st.secrets["ELEVENLAB_API_KEY"],
+)
+
+## Utils
 def main():
     # Set a title
-    st.title("Dynamite: Blow up your business!?")
+    st.title("My LangChain App")
 
-    # Display environment variable information
-    st.write("Anthropic", os.environ["ANTHROPIC_API_KEY"] == st.secrets["ANTHROPIC_API_KEY"])
-    st.write("Openai:", os.environ["OPENAI_API_KEY"] == st.secrets["OPENAI_API_KEY"])    
-    st.write("Elevenlabs:", os.environ["ELEVENLABS_API_KEY"] == st.secrets["ELEVENLABS_API_KEY"])    
-            
-
-    # Select between the translation, chat, and audio pages
-    page = st.sidebar.selectbox("Choose a page:", ["Translation", "Chat", "Audio"])
+    # Select between the translation, chat, and audio recording pages
+    page = st.sidebar.selectbox("Choose a page:", ["Translation", "Chat", "Audio Recording"])
 
     if page == "Translation":
         translation_page()
     elif page == "Chat":
         chat_page()
-    elif page == "Audio":
-        audio_page()
+    elif page == "Audio Recording":
+        audio_recording_page()
 
 
-def audio_page():
-    # Generate an audio signal
-    audio_signal, sample_rate = generate_audio()
+def audio_recording_page():
+    wav_audio_data = st_audiorec()
 
-    # Display an audio player
-    st.audio(audio_signal, format='audio/wav', start_time=0, sample_rate=sample_rate)
-
+    if wav_audio_data is not None:
+        st.audio(wav_audio_data, format='audio/wav')
 
 if __name__ == "__main__":
     main()
